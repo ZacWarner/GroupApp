e4a1f3265375bdf3dcc98d353ad5e24e3ee12975
MeenaKrs <46361811+MeenaKrs@users.noreply.github.com>
2019-04-27 11:05:09 -0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,87 @@
-var dealsCount = 0;
-var queryUrl = "https://api.discountapi.com/v2/deals?api_key=nvWHzpcy&query=food&location=fair+oaks&radius=5&category-slugs=food-grocery,food-alcohol";
-$.ajax({
-    url: queryUrl,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-    console.log(response.query.total);
-    console.log(response.deals[0].deal.short_title);
-    console.log(response.deals[0].deal.image_url);
-    console.log(response.deals[0].deal.expires_at);
-    var expr = response.deals[0].deal.expires_at;
-    var newDate = moment(expr).format("Do MMM dddd");
-    console.log(newDate);
-    console.log(response.deals[0].deal.merchant.name);
-    console.log(response.deals[0].deal.merchant.address);
-    console.log(response.deals[0].deal.merchant.locatlity);
-    console.log(response.deals[0].deal.merchant.region);
-    console.log(response.deals[0].deal.merchant.postal_code);
-    console.log(response.deals[0].deal.merchant.country);
+var categoryCount = 0;
+function populateDealCategory() {
+    for (let i = 0; i < dealCategories.length; i++) {
+        var newOption = $("<option>");
+        newOption.text(dealCategories[i].category.name);
+        $(".category").append(newOption);
+    }
+}
 
-    var dealDiv = $("<div>");
-    var title = $("<div>");
-    title.html(response.deals[0].deal.short_title + "<br>");
-    title.append("Expires at: " + response.deals[0].deal.expires_at);
-    var img = $("<img>");
-    img.attr("src", response.deals[0].deal.image_url);
-    var addr = $("<div>");
-    addr.html("<p>" + response.deals[0].deal.merchant.name + "," + response.deals[0].deal.merchant.address + "," + response.deals[0].deal.merchant.locatlity + "," + response.deals[0].deal.merchant.region + "," + response.deals[0].deal.merchant.country + "-" + response.deals[0].deal.merchant.postal_code);
-    dealDiv.append(title, img, addr);
+function grabDeals(queryUrl) {
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        $(".lead").html(" ");
+        for (let i = 0; i < response.deals.length; i++) {
+            var expr = response.deals[i].deal.expires_at;
+            var newDate = moment(expr).format("Do MMM dddd");
 
-    $(".carousel").append(dealDiv);
+            var dealDiv = $("<div>");
+            dealDiv.addClass("card bg-warning slide");
+            dealDiv.attr("style", "width: 18rem;");
+
+            var title = $("<div>");
+            title.html(response.deals[i].deal.short_title);
+            title.addClass("card-title");
+
+            var img = $("<img>");
+            img.addClass("card-img-top image");
+            img.attr("src", response.deals[i].deal.image_url);
+            img.attr("width", "200");
+            img.attr("height", "150");
+            img.attr("max-height", "200");
+
+            var subDealDiv = $("<div>");
+            subDealDiv.addClass("middle");
+
+            var addr = $("<div>");
+            addr.addClass("text");
+            addr.html("<p>" + response.deals[i].deal.merchant.name + "," + response.deals[i].deal.merchant.address + "," + response.deals[i].deal.merchant.locality + "," + response.deals[i].deal.merchant.region + "-" + response.deals[i].deal.merchant.postal_code);
+
+            var expire = $("<p>");
+            expire.html("<b><i>Valid till " + newDate + "</i></b>");
+            expire.addClass("deal-validity text");
+
+            subDealDiv.append(addr, expire);
+
+            dealDiv.append(img, title, subDealDiv);
+
+            $(".slider").prepend(dealDiv);
+
+        }
+        $('.slider').slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+        });
+    });
+}
+
+$(document).ready(function () {
+
+    populateDealCategory();
+
+    $("select.category").change(function () {
+        categoryCount++;
+        if (categoryCount < 4) {
+            var selectedCategory = $(this).children("option:selected").val();
+            $(".item-" + categoryCount).text(selectedCategory);
+        }
+    });
+
+    $("#find-deals").on("click", function (event) {
+        event.preventDefault();
+
+        var tmp = $("li.list-group-item");
+        var category1 = tmp[0].innerText;
+        var category2 = tmp[1].innerText;
+        var category3 = tmp[2].innerText;
+
+        var queryUrl = "https://api.discountapi.com/v2/deals?api_key=nvWHzpcy&query=" + category1 + "+" + category2 + "+" + category3 + "&location=sacramento&radius=5";
+        grabDeals(queryUrl);
+    });
+
 });
---
+++ assets/js/dealCategories.js
@@ -0,0 +1,561 @@
+var dealCategories = [
+    {
+        "category": {
+            "slug": "activities-events",
+            "name": "Activities & Events"
+        }
+    },
+    {
+        "category": {
+            "slug": "adult",
+            "name": "Adult Products",
+            "parent_slug": "product"
+        }
+    },
+    {
+        "category": {
+            "slug": "audio",
+            "name": "Audio & Accessories",
+            "parent_slug": "product"
+        }
+    },
+    {
+        "category": {
+            "slug": "automotive",
+            "name": "Automotive",
+            "parent_slug": "product"
+        }
+    },
+    {
+        "category": {
+            "slug": "automotive-services",
+            "name": "Automotive Services",
+            "parent_slug": "retail-services"
+        }
+    },
+    {
+        "category": {
+            "slug": "baby",
+            "name": "Baby",
+            "parent_slug": "special-interest"
+        }
+    },
+    {
+        "category": {
+            "slug": "bars-clubs",
+            "name": "Bars & Clubs",
+            "parent_slug": "dining-nightlife"
+        }
+    },
+    {
+        "category": {
+            "slug": "beauty_health",
+            "name": "Beauty & Health",
+            "parent_slug": "product"
+        }
+    },
+    {
+        "category": {
+            "slug": "boot-camp",
+            "name": "Boot Camp",
+            "parent_slug": "fitness"
+        }
+    },
+    {
+        "category": {
+            "slug": "bowling",
+            "name": "Bowling",
+            "parent_slug": "activities-events"
+        }
+    },
+    {
+        "category": {
+            "slug": "bridal",
+            "name": "Bridal",
+            "parent_slug": "special-interest"
+        }
+    },
+    {
+        "category": {
+            "slug": "chiropractic",
+            "name": "Chiropractic",
+            "parent_slug": "health-beauty"
+        }
+    },
+    {
+        "category": {
+            "slug": "city-tours",
+            "name": "City Tours",
+            "parent_slug": "activities-events"
+        }
+    },
+    {
+        "category": {
+            "slug": "clothing",
+            "name": "Clothing",
+            "parent_slug": "retail-services"
+        }
+    },
+    {
+        "category": {
+            "slug": "college",
+            "name": "College",
+            "parent_slug": "special-interest"
+        }
+    },
+    {
+        "category": {
+            "slug": "comedy-clubs",
+            "name": "Comedy Clubs",
+            "parent_slug": "activities-events"
+        }
+    },
+    {
+        "category": {
+            "slug": "concerts",
+            "name": "Concerts",
+            "parent_slug": "activities-events"
+        }
+    },
+    {
+        "category": {
+            "slug": "crafts_hobbies",
+            "name": "Crafts & Hobbies",
+            "parent_slug": "product"
+        }
+    },
+    {
+        "category": {
+            "slug": "dance-classes",
+            "name": "Dance Classes",
+            "parent_slug": "activities-events"
+        }
+    },
+    {
+        "category": {
+            "slug": "dental",
+            "name": "Dental",
+            "parent_slug": "health-beauty"
+        }
+    },
+    {
+        "category": {
+            "slug": "dermatology",
+            "name": "Dermatology",
+            "parent_slug": "health-beauty"
+        }
+    },
+    {
+        "category": {
+            "slug": "dining-nightlife",
+            "name": "Dining & Nightlife"
+        }
+    },
+    {
+        "category": {
+            "slug": "electronics",
+            "name": "Electronics",
+            "parent_slug": "product"
+        }
+    },
+    {
+        "category": {
+            "slug": "eye-vision",
+            "name": "Eye & Vision",
+            "parent_slug": "health-beauty"
+        }
+    },
+    {
+        "category": {
+            "slug": "facial",
+            "name": "Facial",
+            "parent_slug": "health-beauty"
+        }
+    },
+    {
+        "category": {
+            "slug": "fashion_accessories",
+            "name": "Fashion Accessories",
+            "parent_slug": "product"
+        }
+    },
+    {
+        "category": {
+            "slug": "fitness",
+            "name": "Fitness"
+        }
+    },
+    {
+        "category": {
+            "slug": "fitness_product",
+            "name": "Fitness",
+            "parent_slug": "product"
+        }
+    },
+    {
+        "category": {
+            "slug": "fitness-classes",
+            "name": "Fitness Classes",
+            "parent_slug": "fitness"
+        }
+    },
+    {
+        "category": {
+            "slug": "food_alcohol",
+            "name": "Food & Alcohol",
+            "parent_slug": "product"
+        }
+    },
+    {
+        "category": {
+            "slug": "food-grocery",
+            "name": "Food & Grocery",
+            "parent_slug": "retail-services"
+        }
+    },
+    {
+        "category": {
+            "slug": "gay",
+            "name": "Gay",
+            "parent_slug": "special-interest"
+        }
+    },
+    {
+        "category": {
+            "slug": "gifts",
+            "name": "Gift Ideas",
+            "parent_slug": "product"
+        }
+    },
+    {
+        "category": {
+            "slug": "golf",
+            "name": "Golf",
+            "parent_slug": "activities-events"
+        }
+    },
+    {
+        "category": {
+            "slug": "gym",
+            "name": "Gym",
+            "parent_slug": "fitness"
+        }
+    },
+    {
+        "category": {
+            "slug": "hair-removal",
+            "name": "Hair Removal",
+            "parent_slug": "health-beauty"
+        }
+    },
+    {
+        "category": {
+            "slug": "hair-salon",
+            "name": "Hair Salon",
+            "parent_slug": "health-beauty"
+        }
+    },
+    {
+        "category": {
+            "slug": "health-beauty",
+            "name": "Health & Beauty"
+        }
+    },
+    {
+        "category": {
+            "slug": "home_goods",
+            "name": "Home Goods",
+            "parent_slug": "product"
+        }
+    },
+    {
+        "category": {
+            "slug": "home-services",
+            "name": "Home Services",
+            "parent_slug": "retail-services"
+        }
+    },
+    {
+        "category": {
+            "slug": "jewish",
+            "name": "Jewish",
+            "parent_slug": "special-interest"
+        }
+    },
+    {
+        "category": {
+            "slug": "kids",
+            "name": "Kids",
+            "parent_slug": "special-interest"
+        }
+    },
+    {
+        "category": {
+            "slug": "kitchen",
+            "name": "Kitchen",
+            "parent_slug": "product"
+        }
+    },
+    {
+        "category": {
+            "slug": "kosher",
+            "name": "Kosher",
+            "parent_slug": "special-interest"
+        }
+    },
+    {
+        "category": {
+            "slug": "life-skills-classes",
+            "name": "Life Skills Classes",
+            "parent_slug": "activities-events"
+        }
+    },
+    {
+        "category": {
+            "slug": "luggage",
+            "name": "Luggage & Baggage",
+            "parent_slug": "product"
+        }
+    },
+    {
+        "category": {
+            "slug": "makeup",
+            "name": "Makeup",
+            "parent_slug": "health-beauty"
+        }
+    },
+    {
+        "category": {
+            "slug": "manicure-pedicure",
+            "name": "Manicure & Pedicure",
+            "parent_slug": "health-beauty"
+        }
+    },
+    {
+        "category": {
+            "slug": "martial-arts",
+            "name": "Martial Arts",
+            "parent_slug": "fitness"
+        }
+    },
+    {
+        "category": {
+            "slug": "massage",
+            "name": "Massage",
+            "parent_slug": "health-beauty"
+        }
+    },
+    {
+        "category": {
+            "slug": "mens-clothing",
+            "name": "Men's Clothing",
+            "parent_slug": "clothing"
+        }
+    },
+    {
+        "category": {
+            "slug": "mens_fashion",
+            "name": "Men's Fashion",
+            "parent_slug": "product"
+        }
+    },
+    {
+        "category": {
+            "slug": "mobile",
+            "name": "Mobile Devices & Accessories",
+            "parent_slug": "product"
+        }
+    },
+    {
+        "category": {
+            "slug": "movies_music_games",
+            "name": "Movies, Music, & Games",
+            "parent_slug": "product"
+        }
+    },
+    {
+        "category": {
+            "slug": "museums",
+            "name": "Museums",
+            "parent_slug": "activities-events"
+        }
+    },
+    {
+        "category": {
+            "slug": "office_supplies",
+            "name": "Office Supplies",
+            "parent_slug": "product"
+        }
+    },
+    {
+        "category": {
+            "slug": "outdoor-adventures",
+            "name": "Outdoor Adventures",
+            "parent_slug": "activities-events"
+        }
+    },
+    {
+        "category": {
+            "slug": "personal-training",
+            "name": "Personal Training",
+            "parent_slug": "fitness"
+        }
+    },
+    {
+        "category": {
+            "slug": "pets",
+            "name": "Pets",
+            "parent_slug": "special-interest"
+        }
+    },
+    {
+        "category": {
+            "slug": "photography-services",
+            "name": "Photography Services",
+            "parent_slug": "retail-services"
+        }
+    },
+    {
+        "category": {
+            "slug": "pilates",
+            "name": "Pilates",
+            "parent_slug": "fitness"
+        }
+    },
+    {
+        "category": {
+            "slug": "product",
+            "name": "Product"
+        }
+    },
+    {
+        "category": {
+            "slug": "restaurants",
+            "name": "Restaurants",
+            "parent_slug": "dining-nightlife"
+        }
+    },
+    {
+        "category": {
+            "slug": "retail-services",
+            "name": "Retail & Services"
+        }
+    },
+    {
+        "category": {
+            "slug": "skiing",
+            "name": "Skiing",
+            "parent_slug": "activities-events"
+        }
+    },
+    {
+        "category": {
+            "slug": "skydiving",
+            "name": "Skydiving",
+            "parent_slug": "activities-events"
+        }
+    },
+    {
+        "category": {
+            "slug": "spa",
+            "name": "Spa",
+            "parent_slug": "health-beauty"
+        }
+    },
+    {
+        "category": {
+            "slug": "special-interest",
+            "name": "Special Interest"
+        }
+    },
+    {
+        "category": {
+            "slug": "sporting-events",
+            "name": "Sporting Events",
+            "parent_slug": "activities-events"
+        }
+    },
+    {
+        "category": {
+            "slug": "tanning",
+            "name": "Tanning",
+            "parent_slug": "health-beauty"
+        }
+    },
+    {
+        "category": {
+            "slug": "teeth-whitening",
+            "name": "Teeth Whitening",
+            "parent_slug": "health-beauty"
+        }
+    },
+    {
+        "category": {
+            "slug": "theater",
+            "name": "Theater",
+            "parent_slug": "activities-events"
+        }
+    },
+    {
+        "category": {
+            "slug": "tools",
+            "name": "Tools & Hardware",
+            "parent_slug": "product"
+        }
+    },
+    {
+        "category": {
+            "slug": "toys",
+            "name": "Toys",
+            "parent_slug": "product"
+        }
+    },
+    {
+        "category": {
+            "slug": "travel",
+            "name": "Travel",
+            "parent_slug": "special-interest"
+        }
+    },
+    {
+        "category": {
+            "slug": "treats",
+            "name": "Treats",
+            "parent_slug": "retail-services"
+        }
+    },
+    {
+        "category": {
+            "slug": "unknown",
+            "name": "Unkown"
+        }
+    },
+    {
+        "category": {
+            "slug": "wine-tasting",
+            "name": "Wine Tasting",
+            "parent_slug": "activities-events"
+        }
+    },
+    {
+        "category": {
+            "slug": "womens-clothing",
+            "name": "Women's Clothing",
+            "parent_slug": "clothing"
+        }
+    },
+    {
+        "category": {
+            "slug": "women_fashion",
+            "name": "Women's Fashion",
+            "parent_slug": "product"
+        }
+    },
+    {
+        "category": {
+            "slug": "yoga",
+            "name": "Yoga",
+            "parent_slug": "fitness"
+        }
+    }
+];
